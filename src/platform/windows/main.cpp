60d6aa24b503e5deaf0bf44ffc01387d2a27478b
comoco-5672 <775324623@qq.com>
2024-09-15 01:13:23 +0800

    create a window

--- src/platform/windows/main.cpp
+++ src/platform/windows/main.cpp
@@ -1,7 +1,58 @@
+#include <GLFW/glfw3.h>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    if (!glfwInit())
+        return 1;
+
+    GLFWwindow *windows = glfwCreateWindow(800, 600, "Metaphysics", nullptr, nullptr);
+    if (windows == nullptr)
+        return 1;
+    glfwMakeContextCurrent(windows);
+    glfwSwapInterval(1);
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO &io = ImGui::GetIO();
+
+    ImGui::StyleColorsClassic();
+
+    ImGui_ImplGlfw_InitForOpenGL(windows, true);
+    ImGui_ImplOpenGL3_Init("#version 130");
+
+    while (!glfwWindowShouldClose(windows)) {
+        glfwPollEvents();
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+
+        ImGui::NewFrame();
+
+        ImGui::Begin("Hello, ImGui!");
+        ImGui::Text("这是一个 ImGui 窗口");
+        ImGui::End();
+
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(windows, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(windows);
+    }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(windows);
+    glfwTerminate();
+
     return 0;
 }
