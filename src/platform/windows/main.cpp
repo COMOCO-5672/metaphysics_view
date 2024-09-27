34c95a57afa4663222c8cacfbc776af24ad8aec1
comoco-5672 <775324623@qq.com>
2024-09-28 00:50:43 +0800

    ogre init

--- src/platform/windows/main.cpp
+++ src/platform/windows/main.cpp
@@ -1,262 +1,61 @@
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-#include <imgui.h>
-#include <imgui_impl_glfw.h>
-#include <imgui_impl_opengl3.h>
-#include <iostream>
-#include <vector>
+#include <Ogre.h>
+#include <OgreApplicationContext.h>
+#include <OgreInput.h>
+#include <OgreRTShaderSystem.h>
 
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
-
-// glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
-// glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
-// glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
-
-int currentWidth = SCR_WIDTH;
-int currentHeight = SCR_HEIGHT;
-
-glm::vec3 cameraPos = glm::vec3(5.0f, 5.0f, 5.0f);
-glm::vec3 cameraFront = glm::normalize(glm::vec3(-1.0f, -1.0f, -1.0f));
-glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
-
-double lastX = SCR_WIDTH / 2.0f;
-double lastY = SCR_HEIGHT / 2.0f;
-float yaw = -90.0f;
-float pitch = 0.0f;
-bool firstMove = true;
-bool mouse_pressed = false;
-
-const char *vertexShaderSource = R"(
-    #version 330 core
-    layout (location = 0) in vec3 aPos;
-    uniform mat4 model;
-    uniform mat4 view;
-    uniform mat4 projection;
-    void main()
-    {
-        gl_Position = projection * view * model * vec4(aPos.x, aPos.y, aPos.z, 1.0);
-    }
-)";
-
-const char *fragmentShaderSource = R"(
-    #version 330 core
-    out vec4 FragColor;
-    void main()
-    {
-        FragColor = vec4(1.0, 1.0, 1.0, 1.0);
-    }
-)";
-
-void processInput(GLFWwindow *window)
+class TutorialApplication : public OgreBites::ApplicationContext, public OgreBites::InputListener
 {
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
+public:
+    TutorialApplication() : OgreBites::ApplicationContext("OgreApp")
+    {
+    }
 
-    float cameraSpeed = 0.01f;
-    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-        cameraPos += cameraSpeed * cameraFront;
-    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-        cameraPos -= cameraSpeed * cameraFront;
-    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-        cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
-    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
-}
+    void setup() override
+    {
+        OgreBites::ApplicationContext::setup();
+        addInputListener(this);
 
-void mouse_button_callback(GLFWwindow *window, int button, int action, int mods)
-{
-    if (button == GLFW_MOUSE_BUTTON_LEFT) {
-        if (action == GLFW_PRESS) {
-            mouse_pressed = true;
-            firstMove = true;
-            glfwGetCursorPos(window, &lastX, &lastY);
+        Ogre::Root* root = getRoot();
+        Ogre::SceneManager* scnMgr = root->createSceneManager();
+
+        Ogre::RTShader::ShaderGenerator* shadergen = Ogre::RTShader::ShaderGenerator::getSingletonPtr();
+        shadergen->addSceneManager(scnMgr);
+
+        Ogre::Light* light = scnMgr->createLight("MainLight");
+        Ogre::SceneNode* lightNode = scnMgr->getRootSceneNode()->createChildSceneNode();
+        lightNode->attachObject(light);
+        lightNode->setPosition(20, 80, 50);
+
+        Ogre::SceneNode* camNode = scnMgr->getRootSceneNode()->createChildSceneNode();
+        camNode->setPosition(0, 0, 15);
+        camNode->lookAt(Ogre::Vector3(0, 0, -1), Ogre::Node::TS_PARENT);
+
+        Ogre::Camera* cam = scnMgr->createCamera("myCam");
+        cam->setNearClipDistance(5);
+        cam->setAutoAspectRatio(true);
+        camNode->attachObject(cam);
+        getRenderWindow()->addViewport(cam);
+
+        Ogre::Entity* ogreEntity = scnMgr->createEntity("ogrehead.mesh");
+        Ogre::SceneNode* ogreNode = scnMgr->getRootSceneNode()->createChildSceneNode();
+        ogreNode->attachObject(ogreEntity);
+    }
+
+    bool keyPressed(const OgreBites::KeyboardEvent& evt) override
+    {
+        if (evt.keysym.sym == OgreBites::SDLK_ESCAPE)
+        {
+            getRoot()->queueEndRendering();
         }
-        else if (action == GLFW_RELEASE)
-            mouse_pressed = false;
+        return true;
     }
-}
-
-void mouse_callback(GLFWwindow *window, double xpos, double ypos)
-{
-    if (!mouse_pressed)
-        return;
-
-    if (firstMove) {
-        lastX = xpos;
-        lastY = ypos;
-        firstMove = false;
-        return;
-    }
-
-    float xoffset = xpos - lastX;
-    float yoffset = lastY - ypos;
-    lastX = xpos;
-    lastY = ypos;
-
-    float sensitivity = 0.05f;
-    xoffset *= sensitivity;
-    yoffset *= sensitivity;
-
-    yaw += xoffset;
-    pitch += yoffset;
-
-    if (pitch > 89.0f)
-        pitch = 89.0f;
-    if (pitch < -89.0f)
-        pitch = -89.0f;
-
-    glm::vec3 front;
-    front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
-    front.y = sin(glm::radians(pitch));
-    front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
-    cameraFront = glm::normalize(front);
-}
-
-void framebuffer_size_callback(GLFWwindow *window, int width, int height)
-{
-    glViewport(0, 0, width, height);
-    currentWidth = width;
-    currentHeight = height;
-}
-
-void scroll_callback(GLFWwindow *window, double xoffset, double yoffset)
-{
-    float zoom_speed = 0.1f;
-    cameraPos += cameraFront * zoom_speed * (float)yoffset;
-}
+};
 
 int main()
 {
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-    GLFWwindow *window =
-        glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "World Coordinate Plane", NULL, NULL);
-    if (window == NULL) {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-    glfwSetCursorPosCallback(window, mouse_callback);
-    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-    glfwSetMouseButtonCallback(window, mouse_button_callback);
-    glfwSetScrollCallback(window, scroll_callback);
-
-    if (glewInit() != GLEW_OK) {
-        std::cout << "Failed to initialize GLEW" << std::endl;
-        return -1;
-    }
-
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO &io = ImGui::GetIO();
-    (void)io;
-    ImGui::StyleColorsDark();
-    ImGui_ImplGlfw_InitForOpenGL(window, true);
-    ImGui_ImplOpenGL3_Init("#version 330");
-
-    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-
-    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-
-    unsigned int shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-
-    std::vector<float> vertices;
-    for (float i = -10.0f; i <= 10.0f; i += 1.0f) {
-        // X轴平行线
-        vertices.push_back(-10.0f);
-        vertices.push_back(0.0f);
-        vertices.push_back(i);
-        vertices.push_back(10.0f);
-        vertices.push_back(0.0f);
-        vertices.push_back(i);
-
-        // Z轴平行线
-        vertices.push_back(i);
-        vertices.push_back(0.0f);
-        vertices.push_back(-10.0f);
-        vertices.push_back(i);
-        vertices.push_back(0.0f);
-        vertices.push_back(10.0f);
-    }
-
-    unsigned int VBO, VAO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    glBindVertexArray(VAO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
-    glEnableVertexAttribArray(0);
-
-    glEnable(GL_DEPTH_TEST);
-
-    while (!glfwWindowShouldClose(window)) {
-        processInput(window);
-
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        ImGui_ImplOpenGL3_NewFrame();
-        ImGui_ImplGlfw_NewFrame();
-        ImGui::NewFrame();
-
-        glUseProgram(shaderProgram);
-
-        glm::mat4 model = glm::mat4(1.0f);
-        glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
-        glm::mat4 projection = glm::perspective(
-            glm::radians(45.0f), (float)currentWidth / (float)currentHeight, 0.1f, 100.0f);
-
-        unsigned int modelLoc = glGetUniformLocation(shaderProgram, "model");
-        unsigned int viewLoc = glGetUniformLocation(shaderProgram, "view");
-        unsigned int projectionLoc = glGetUniformLocation(shaderProgram, "projection");
-
-        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-        glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
-
-        glBindVertexArray(VAO);
-        glDrawArrays(GL_LINES, 0, vertices.size() / 3);
-
-        ImGui::Begin("Hello, ImGui!");
-        ImGui::Text("this is ImGui window");
-
-        ImGui::End();
-
-        ImGui::Render();
-        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
-    glDeleteProgram(shaderProgram);
-
-    ImGui_ImplOpenGL3_Shutdown();
-    ImGui_ImplGlfw_Shutdown();
-    ImGui::DestroyContext();
-
-    glfwTerminate();
+    TutorialApplication app;
+    app.initApp();
+    app.getRoot()->startRendering();
+    app.closeApp();
     return 0;
 }
